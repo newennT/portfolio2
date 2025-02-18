b175a783c1110c8c2efb76ef62442ea1137718af
newennT <newenn.turbiau@gmail.com>
2025-02-18 16:03:45 +0100

    Animation Accueil

--- assets/app.js
+++ assets/app.js
@@ -10,5 +10,6 @@ import './js/body.js';
 import './js/header.js';
 import './js/projets.js';
 import './js/apparition-projet.js';
+import './js/accueil.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ assets/js/accueil.js
@@ -0,0 +1,36 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tlAccueil01 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section02",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        toggleActions: "play none none reverse"
+    }
+});
+
+tlAccueil01.to("#accueil01", {
+    scale: 1.1,
+    y: 300,
+    duration: 4,
+    ease: "power1.inOut"
+});
+
+const tlAccueil02 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section02",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        toggleActions: "play none none reverse"
+    }
+});
+
+tlAccueil02.to("#accueil02", {
+    scale: 1.1,
+    y: 400,
+    duration: 4,
+    ease: "power1.out"
+});
+
