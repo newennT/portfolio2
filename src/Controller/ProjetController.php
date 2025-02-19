f648dee80721cf868e5aba25747a45d3aee5bc58
newennT <newenn.turbiau@gmail.com>
2025-02-19 14:42:48 +0100

    Filtres portfolio

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Projet;
 
 use App\Repository\ProjetRepository;
+use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,10 +14,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProjetController extends AbstractController
 {
     #[Route(name: 'app_projet_index', methods: ['GET'])]
-    public function index(ProjetRepository $projetRepository): Response
+    public function index(ProjetRepository $projetRepository, CategorieRepository $categorieRepository): Response
     {
         return $this->render('projet/index.html.twig', [
             'projets' => $projetRepository->findAll(),
+            'categories' => $categorieRepository->findAll(),
         ]);
     }
 
